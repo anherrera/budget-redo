64e41541503aa8dbcc7059fb0f74a657eed89db2
Alexa Herrera <alexa.herrera90@gmail.com>
2024-08-16 15:50:16 -0500

    remove vanilla login

--- imports/ui/login/LoginForm.jsx
+++ imports/ui/login/LoginForm.jsx
@@ -1,55 +1,16 @@
-import { Meteor } from 'meteor/meteor';
-import React, { useState } from 'react';
+import React from 'react';
 import { LoginWithGithub } from "./LoginWithGithub";
 import { LoginWithGoogle } from "./LoginWithGoogle";
-import {Button, Container, FormControl, InputLabel, TextField} from "@mui/material";
+import { Container } from "@mui/material";
 
 export const LoginForm = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const submit = e => {
-        e.preventDefault();
-
-        Meteor.loginWithPassword(username, password);
-    };
-
     return (
-        <Container spacing={2}>
-            <form onSubmit={submit} className="login-form">
+        <Container spacing={2} className="login-form">
 
-                <LoginWithGithub />
-                <br />
-                <LoginWithGoogle />
+            <LoginWithGithub />
+            <br />
+            <LoginWithGoogle />
 
-                <FormControl margin="normal">
-                    <InputLabel htmlFor="username">Username</InputLabel>
-
-                    <TextField
-                        type="text"
-                        placeholder="Username"
-                        name="username"
-                        required
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </FormControl>
-
-                <FormControl margin="normal">
-                    <InputLabel htmlFor="password">Password</InputLabel>
-
-                    <TextField
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        required
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </FormControl>
-
-                <FormControl margin="normal">
-                    <Button type="submit" variant="contained" color="secondary" sx={{padding: "10"}}>Log In</Button>
-                </FormControl>
-            </form>
         </Container>
     );
 };
